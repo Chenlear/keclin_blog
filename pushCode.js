0b555ac427326177be9bd5617ddcce3622edf113
keclin <19122081@qq.com>
2019-09-24 11:22:24 +0800

    测试提交代码自动同步打包部署

--- pushCode.js
+++ pushCode.js
@@ -10,7 +10,7 @@ http.createServer(function (req, res) {
     res.statusCode = 404;
     res.end('no such location')
   })
-}).listen(3000)
+}).listen(8080)
 
 handler.on('error', function (err) {
   console.error('Error:', err.message)
