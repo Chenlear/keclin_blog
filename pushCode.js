e4733cbd1331b2622cc4c13f59906a31d99c7ad7
keclin <19122081@qq.com>
2019-09-24 15:54:17 +0800

    即将起航

--- pushCode.js
+++ pushCode.js
@@ -1,10 +1,12 @@
 var http = require('http')
 var spawn = require('child_process').spawn
 var createHandler = require('github-webhook-handler')
+//此处的path和secret必须和github的webhooks里面的一一对应
 var handler = createHandler({
   path: '/pushAction',
   secret: 'keclin712'
 })
+//创建一个http服务，监听端口3333
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
     res.statusCode = 404;
@@ -16,6 +18,7 @@ handler.on('error', function (err) {
   console.error('Error:', err.message)
 })
 
+//监听push操作，并调用shell脚本文件
 handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
